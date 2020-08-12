de369da5a79adb9884323789f1009fc99a6222ad
Raka <38872863+rkaprasetya@users.noreply.github.com>
2020-08-12 11:35:33 +0700

    first commit

---
+++ app/src/main/java/com/raka/motionlayourapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.raka.motionlayourapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(),OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+
+    }
+}

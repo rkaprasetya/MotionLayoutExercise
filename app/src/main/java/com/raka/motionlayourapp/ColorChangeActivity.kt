c1b31b71212f187d08c15225a78925a01f3f9f47
Raka <38872863+rkaprasetya@users.noreply.github.com>
2020-08-12 14:59:20 +0700

    add more animation

---
+++ app/src/main/java/com/raka/motionlayourapp/ColorChangeActivity.kt
@@ -0,0 +1,11 @@
+package com.raka.motionlayourapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorChangeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_change)
+    }
+}
